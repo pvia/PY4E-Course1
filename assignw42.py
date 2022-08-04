5fb3954939d60c194a2e2302f1798068447f96e3
Prioteasa Anca <109321395+pvia@users.noreply.github.com>
2022-08-04 17:16:09 +0300

    Create assignw42.py

---
+++ assignw42.py
@@ -0,0 +1,7 @@
+# Write a program to prompt the user for hours and rate
+# per hour using input to compute gross pay.
+
+hours=input("Enter hours: ")
+rate=input("Enter rate: ")
+pay=float(hours)*float(rate)
+print("Pay:",pay)
